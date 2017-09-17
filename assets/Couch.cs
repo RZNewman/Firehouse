26a3ec59f3ae6d474df685f4f7c87595c5d8f5ad
Spencer Florence <spencer@florence.io>
2017-09-17 14:35:12 -0500

    couches

--- assets/Couch.cs
+++ assets/Couch.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Couch : Thing {
+	public bool isPassable = false;
+	public bool isFlamable = true;
 
 	// Use this for initialization
 	void Start () {
-		this.isPassable = false;
-		isFlamable = true;
 		
 	}
 	
