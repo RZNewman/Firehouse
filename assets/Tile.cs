591c259114d5d12938f2f2f22c6c428c3ffbec81
RZNewman <rudolfnewman2017@u.northwestern.edu>
2017-09-17 15:14:07 -0500

    direction

--- assets/Tile.cs
+++ assets/Tile.cs
@@ -37,4 +37,37 @@ public class Tile : MonoBehaviour {
 			return (thing == null) || thing.GetComponent<Thing>().isPassable;
         }
     }
+    GameObject relativeTile(House.direction d)
+    {
+        Vector2 dir;
+        switch (d)
+        {
+            case House.direction.up:
+                dir = new Vector2(0, 1);
+                break;
+            case House.direction.down:
+                dir = new Vector2(0, -1);
+                break;
+            case House.direction.left:
+                dir = new Vector2(-1, 0);
+                break;
+            case House.direction.right:
+                dir = new Vector2(1, 0);
+                break;
+            default:
+                dir = Vector2.zero;
+                break;
+        }
+        Vector3 c = dir;
+        Collider2D col = Physics2D.OverlapBox(transform.position + c, new Vector2(0.2f, 0.2f), 0);
+        if (!col)
+        {
+            return null;
+        }
+        else
+        {
+            return col.gameObject;
+        }
+
+    }
 }
