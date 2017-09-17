a50438208b75bcf6c125f1279d7b2eba3bdbe3ab
Spencer Florence <spencer@florence.io>
2017-09-17 16:30:44 -0500

    death

--- assets/Player.cs
+++ assets/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public GameObject tile;
+	public int health = 10;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +13,14 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	public void tick() {
+		if (this.tile.GetComponent<Tile> ().fire != 0) {
+			health -= 1;
+		}
+		if (health <= 0) {
+			this.gameObject.SetActive (false);
+			print ("DEAD");
+		}
 	}
 }
