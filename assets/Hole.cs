b3e55e875a3f0cc2fe0ad8fb05ced25768f4433b
Spencer Florence <spencer@florence.io>
2017-09-17 14:46:24 -0500

    add forgotten code

---
+++ assets/Hole.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : Thing {
+	public bool isPassable = false;
+	public bool isFlamable = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void tick(){}
+}
